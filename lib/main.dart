4cdbd2ac54d6257299ddf5174bb8b7d0b96bf99c
Jefferson Machado <jefferson.machado@gmail.com>
2022-09-29 17:46:25 -0300

    Inicialização do arquivo main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    home: Conversor(),
+  ));
+}
+
+class Conversor extends StatelessWidget {
+  const Conversor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
