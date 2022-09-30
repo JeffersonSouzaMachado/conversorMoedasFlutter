9b9442f312aa36c97a06041eb57a5fb01c4d84c6
Jefferson Machado <jefferson.machado@gmail.com>
2022-09-30 15:12:13 -0300

    Foi criado a interface e carregado o arquivo JSON.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,84 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=d8754823";
 
 void main(List<String> args) {
-  runApp(const MaterialApp(
-    home: Conversor(),
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        // ignore: prefer_const_constructors
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white)),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: const TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
-class Conversor extends StatelessWidget {
-  const Conversor({super.key});
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ double dolar = 0;
+ double euro = 0;
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          '\$ Conversor de Moedas \$',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, AsyncSnapshot) {
+            switch (AsyncSnapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return const Center(
+                  child: Text(
+                    "Carregando Dados",
+                    style: TextStyle(color: Colors.amber, fontSize: 30),
+                  ),
+                );
+              default:
+                if (AsyncSnapshot.hasError) {
+                  const Center(
+                    child: Text(
+                      "Erro ao carregar dados",
+                      style: TextStyle(color: Colors.amber, fontSize: 30),
+                    ),
+                  );
+                }
+                return Container(
+                  dolar = AsyncSnapshot.data!["results"]["currency"]["USD"];
+                  color: Colors.red,
+                );
+            }
+          }),
+    );
   }
 }
